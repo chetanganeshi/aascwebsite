0a010d2bdbdbaccfd3cf9a2a505bcd344a4367ef
Chetan Kumar G <53119829+chetanganeshi@users.noreply.github.com>
2021-05-01 20:31:48 +0530

    Update registration-form.php
    removed email information

--- registration-form.php
+++ registration-form.php
@@ -49,54 +49,8 @@ else {
 	$query = mysqli_query($mysqli, $sql);
 	
 	if ($query === TRUE) {
-				// Load Composer's autoloader
-				require '../../phpMyAdmin/vendor/autoload.php';
-
-				// Instantiation and passing `true` enables exceptions
-				$mail = new PHPMailer(true);
-
-				try {
-				    //Server settings
-				    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-				    $mail->isSMTP();                                            // Send using SMTP
-				    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-				    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-				    $mail->Username   = 'truthspyofficial@gmail.com';                     // SMTP username
-				    $mail->Password   = 'TruthSpy@123';                               // SMTP password
-				    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-				    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-				
-				    //Recipients
-				    $mail->setFrom('truthspyofficial@gmail.com', 'Admin-AASC');
-				    $mail->addAddress($emailid, $name);     // Add a recipient
-				    //$mail->addAddress('truthspyofficial@gmail.com');               // Name is optional
-				    $mail->addReplyTo('truthspyofficial@gmail.com', 'Admin-AASC');
-				    //$mail->addCC('cc@example.com');
-				    //$mail->addBCC('bcc@example.com');
-				
-				    // Attachments
-				    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-				    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-				
-				    // Content
-				    $mail->isHTML(true);                                  // Set email format to HTML
-				    $mail->Subject = "AASC - Account - Registration";
-					
-					ob_start();
-					include('emailtemplates/registration-template.php');
-					$mail->Body = ob_get_contents();
-					ob_end_clean();
-				    
-				
-				    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-				
-				    $mail->send();
-		
-					echo '<script> alert("Data Submitted! Check your registered email for more details. You can login after your account get approved!");</script>'; 
-					echo "<script>window.location.href='index.php';</script>";
-					} catch (Exception $e) {
-				    echo 'Message could not be sent. Error: $mail->ErrorInfo';
-					}
+		echo '<script> alert("Data Submitted! You can login after your account get approved!");</script>'; 
+		echo "<script>window.location.href='index.php';</script>";
 	}
 	else {
 		printf ("could not inserted record: %s \n", mysqli_error($mysqli));
@@ -104,31 +58,6 @@ else {
 }
      
 ?>
-
-<!--<?php 
-$to = $emailid;
-
-$message = "Name: ".$name;
-$message .= "Password: ".$pass;
-
-$subject = "Registration Successfull..!";
-
-$mailheaders = "From: AdoniCollege <website@college.com> \n";
-$mailheaders .= "Reply-To: ".$emailid;
-
-if(mail($to, $subject, $message, $mailheaders))
-echo 'success';
-else
-echo '<h1>failure</h1>';
-?>
-
-ob_start();
-include('invoice.php');
-$mail->Body = ob_get_contents();
-ob_end_clean();
-
-
--->
 
 <script type="text/javascript">
 
