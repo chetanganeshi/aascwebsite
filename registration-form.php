9e1e65c57f178d67908107bd8d575288eb7eaa46
Chetan Kumar G <53119829+chetanganeshi@users.noreply.github.com>
2021-05-01 20:32:17 +0530

    Update registration-form.php

--- registration-form.php
+++ registration-form.php
@@ -1,12 +1,4 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-
 
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
